3758a295d43877491f044ca6292ed2d2b46e4816
brianH <brian.hulbert@me.com>
2018-02-15 14:54:53 -0500

    display Update

--- display.java
+++ display.java
@@ -3,19 +3,24 @@ import java.awt.*;
 public class display extends JPanel
 {
     String view = "";
-    
+
     public void drawing()
     {
         repaint();
     }
+
     public void setView(String v)
     {
         view = v;
     }
+
     public void paintComponent(Graphics g)
     {
         super.paintComponent(g);
-        g.setColor(Color.RED);
-        g.fillRect(10,10,10,10);
+        if(view.equals("login"))
+        {
+            g.setColor(Color.BLACK);
+            g.drawRect(480,255,960,510);
+        }
     }
 }
