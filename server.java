2b1c9d08e2e3d14ed5fa2da816c35f42e1d7d9d0
Tristan <tblonswa@syr.edu>
2018-02-15 22:49:26 -0500

    server tweaks

---
+++ server.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.net.*;
+
+
+
+public class server {
+    //basically manages the entire server
+    
+    
+    
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}
