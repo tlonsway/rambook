51f06a996b9f8090e2044aa46372baeff00bf7d3
Tristan <tblonswa@syr.edu>
2018-02-16 23:51:09 -0500

    Finished client to server continuous transmission - data can now be processed for actual function

--- Server.java
+++ Server.java
@@ -3,22 +3,25 @@ import java.io.*;
 
 public class Server {
     public static void main(String[] args) throws Exception {
-        ServerSocket serversocket = new ServerSocket(4445);
         Server svr = new Server();
-        while(true)
         svr.run();
     }
     public void run() throws Exception {
-        ServerSocket serversocket = new ServerSocket(4445);
+        ServerSocket serversocket = new ServerSocket(5555);
         Socket sock = serversocket.accept();
         InputStreamReader ir = new InputStreamReader(sock.getInputStream());
         BufferedReader br = new BufferedReader(ir);
-        
-        String message = br.readLine();
-        System.out.println(message);
-        if (message != null) {
-            PrintStream ps = new PrintStream(sock.getOutputStream());
-            ps.println("MESSAGE RECEIVED");
+        while(true) {
+            ir = new InputStreamReader(sock.getInputStream());
+            br = new BufferedReader(ir);
+            System.out.println(br.readLine());
         }
+        
+        //String message = br.readLine();
+        //System.out.println(message);
+        //if (message != null) {
+        //    PrintStream ps = new PrintStream(sock.getOutputStream());
+        //    ps.println("MESSAGE RECEIVED");
+        //}
     }
 }
--- client.java
+++ client.java
@@ -1,28 +1,30 @@
 import java.io.*;
 import java.net.*;
-
+import cs1.*;
 public class client {
-    Socket sock;
     public static void main(String[] args) throws Exception {
         client client = new client();
-        client.initialize();
         while(true)
         client.run();
     }
     public void run() throws Exception {
+        Socket sock = new Socket("127.0.0.1", 5555);        
         PrintStream ps = new PrintStream(sock.getOutputStream());
         ps.println("Hello to SERVER from CLIENT");
-        ps.println("Second message");
+        String message;
+        while(true) {
+            message = Keyboard.readString();
+            ps.println(message);
+        }
         
-        InputStreamReader ir = new InputStreamReader(sock.getInputStream());
-        BufferedReader br = new BufferedReader(ir);
         
-        String message = br.readLine();
-        System.out.println(message);
         
-    }
-    public void initialize() throws Exception {
-        sock = new Socket("127.0.0.1", 4445);
+        //InputStreamReader ir = new InputStreamReader(sock.getInputStream());
+        //BufferedReader br = new BufferedReader(ir);
+        
+        //String message = br.readLine();
+        //System.out.println(message);
+        
     }
 }
         
