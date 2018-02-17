9d1f81c818a1cf05f138135ad645631dc37341fe
brian <1@1.1>
2018-02-16 19:17:35 -0500

    tester class for user class, all methods work , no questions

--- UserTester.java
+++ UserTester.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class UserTester
 {
     public static void main(String[] args)
@@ -5,9 +6,23 @@ public class UserTester
         String[] schooolz = new String[5];
         User test1 = new User("brian", 21, "Atlanta gorge",schooolz );
         User test2 = new User("tristan", 5, "Dewitt", schooolz);
+        User test3 = new User("sam" , 20, "jamesville", schooolz);
         test1.addFriend(test2);
         System.out.println(test1.toString());;
+        test2.addFriend(test3);
         test1.unfriend(test2.getName());
         System.out.println(test1.toString());
+        test1.addFriend(test2);
+        System.out.println("Num Friends: " + test1.countFriends());
+        System.out.println("Does test1 equal test2: " + test1.equals(test2));
+        ArrayList<User> mutualFriends = test1.getMutualFriends(test1);
+        System.out.println("test1 mutual friends: " + mutualFriends.get(0).getName());
+        System.out.println(test2.toString());
+        User test4 = new User("bob",30, "Dewitt", schooolz);
+        test4.addFriend(test2);
+        test4.addFriend(test3);
+        mutualFriends.add(test4.getHometownFriends(test4).get(0));
+        System.out.println("Hometown friends of test4: " + mutualFriends.get(1).getName());
+        System.out.println("Suggested friend of test1: " + test1.suggestAFriend().getName());
     }
 }   
