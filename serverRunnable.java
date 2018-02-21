e532e9f688d9d244c94200f1f1395340a5e60fb5
Tristan <tblonswa@syr.edu>
2018-02-21 15:49:57 -0500

    Added more database management functions, added backend user addition and password checking

--- serverRunnable.java
+++ serverRunnable.java
@@ -21,7 +21,8 @@ public class serverRunnable implements Runnable{
         } catch (Exception e) {
             e.printStackTrace();
         }
-        if (line.indexOf(":") != -1) { 
+        
+        if (line.substring(0,1).indexOf("g") != -1) { 
             System.out.println("Message recieved: " + line + "|");
             String input = line;
             int number = -9999;
@@ -51,50 +52,80 @@ public class serverRunnable implements Runnable{
                     e.printStackTrace();
                 }
             }
-            if (action.equals("p")) {
-                try {
-                    ret = checkPassword(name, type);
-                    System.out.println(ret);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
             try {
                 ps.println(new String());
                 ps.println(ret);
             } catch (Exception e) {
                 e.printStackTrace();
             }
-        }
+        } //Decoding get requests, grabbing data, and replying
+        
+        if (line.substring(0,1).indexOf("a") != -1) {
+            //format - a:u:username:name:age:bio
+            String input = line;
+            if (line.substring(2,3).indexOf("u") != -1) {
+                String action = input.substring(0,input.indexOf(":"));
+                input = input.substring(input.indexOf(":")+1);
+                String type = input.substring(0, input.indexOf(":"));
+                input = input.substring(input.indexOf(":")+1);
+                String username = input.substring(0, input.indexOf(":"));
+                input = input.substring(input.indexOf(":")+1);
+                String name = input.substring(0, input.indexOf(":"));
+                input = input.substring(input.indexOf(":")+1);
+                String age = input.substring(0, input.indexOf(":"));
+                input = input.substring(input.indexOf(":")+1);
+                String bio = input;
+                try {
+                    addUser(username, name, Integer.parseInt(age), bio);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }   
+        } //Decoding post tranmissions
+        
+        if (line.substring(0,1).indexOf("p") != -1) {
+            //format - p:username:hash
+            String input = line.substring(line.indexOf(":")+1);
+            String username = input.substring(0, input.indexOf(":"));
+            input = input.substring(input.indexOf(":")+1);
+            String hash = input;
+            String ret = "";
+            try {
+                ret = checkPassword(username, hash);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            ps.println(ret);
+        } //Decode and reply to password checks
     }
     public String getData(String name, String type) throws Exception{
-    FileReader fr = new FileReader("users.txt");
-    BufferedReader br = new BufferedReader(fr);
+        FileReader fr = new FileReader("users.txt");
+        BufferedReader br = new BufferedReader(fr);
         String line;
-    int amt;
+        int amt;
         while(!br.readLine().equals(name)) {
-    }
-    if (type.equals("s")) {
-        amt = 0;
-    } else 
-    if (type.equals("n")) {
-        amt = 1;
-    } else
-    if (type.equals("f")) {
-        amt = 2;
-    } else
-    if (type.equals("a")) {
-        amt = 3;
-    } else {
-        amt = 4;
-    }
-    for (int i=0; i<=amt-1; i++) {
-        br.readLine();
-    }
-    line = br.readLine();
-    System.out.println("line: " + line);
-    String ret = line.substring(line.indexOf(":")+1);
-    return ret;
+        }
+        if (type.equals("s")) {
+            amt = 0;
+        } else 
+        if (type.equals("n")) {
+            amt = 1;
+        } else
+        if (type.equals("f")) {
+            amt = 2;
+        } else
+        if (type.equals("a")) {
+            amt = 3;
+        } else {
+            amt = 4;
+        }
+        for (int i=0; i<=amt-1; i++) {
+            br.readLine();
+        }
+        line = br.readLine();
+        System.out.println("line: " + line);
+        String ret = line.substring(line.indexOf(":")+1);
+        return ret;
     }    
     public String checkPassword(String name, String hash) throws Exception {
         BufferedReader br = new BufferedReader(new FileReader("loginlist.txt"));
@@ -109,5 +140,19 @@ public class serverRunnable implements Runnable{
             }
         }
         return "false"; //returns false if the given user:pass is invalid
+    }
+    public void addUser(String username, String name, int age, String bio) throws Exception{
+        BufferedWriter bw = new BufferedWriter(new FileWriter("users.txt", true));
+        bw.append(username);
+        bw.append("\nstatus:offline");
+        bw.append("\nname:" + name);
+        bw.append("\nfriends:0");
+        bw.append("\nage:" + age);
+        bw.append("\nbio:" + bio);
+        bw.append("\n}");
+        bw.close();
+    }
+    public void setStatus() {
+        
     }
 }
