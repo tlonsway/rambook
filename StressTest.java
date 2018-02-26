60c6815c737dadc061b483af1a033a68fb2a06ff
Tristan <tblonswa@syr.edu>
2018-02-26 14:28:12 -0500

    Adding stress tester and changed ip for different LAN testing

---
+++ StressTest.java
@@ -0,0 +1,11 @@
+public class StressTest {
+    public static void main(String[] args) {
+        while(true) {
+        (new client()).getData("tristan", "bio", 0);
+        (new client()).getData("wez", "status", 0);
+        (new client()).checkPassword(
+        
+    }
+    }
+    
+}
